a4d0079cb788129fe2d1ab3e5c5d702e42b59873
ryomataichi <ryomaryoma0914@gmail.com>
2026-04-10 07:12:29 +0900

    a

--- db/migrate/20260409122538_create_shizens.rb
+++ db/migrate/20260409122538_create_shizens.rb
@@ -1,16 +1,18 @@
 class CreateShizens < ActiveRecord::Migration[7.1]
   def change
-    create_table :shizens do |t|
-      t.string :spot_name
-      t.text :setumei
-      t.string :ken
-      t.string :maplink
-      t.float :latitude, precision: 10, scale: 6
-      t.float :longitude, precision: 10, scale: 6
-      t.string :hourly_weather_type
-      t.datetime :hourly_weather_at
-      t.datetime :hourly_weather_checked_at
-      t.timestamps
+    unless table_exists?(:shizens)
+      create_table :shizens do |t|
+        t.string :spot_name
+        t.text :setumei
+        t.string :ken
+        t.string :maplink
+        t.float :latitude, precision: 10, scale: 6
+        t.float :longitude, precision: 10, scale: 6
+        t.string :hourly_weather_type
+        t.datetime :hourly_weather_at
+        t.datetime :hourly_weather_checked_at
+        t.timestamps
+      end
     end
   end
 end
