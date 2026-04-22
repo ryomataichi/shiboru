b3ee5ed1499418aea0231c92f850f455b513a644
ryomataichi <ryomaryoma0914@gmail.com>
2026-04-22 14:39:53 +0900

    a

--- app/controllers/sitemaps_controller.rb
+++ app/controllers/sitemaps_controller.rb
@@ -2,8 +2,12 @@ class SitemapsController < ApplicationController
   def refresh
     return head :unauthorized unless params[:key] == ENV["SITEMAP_SECRET"]
 
-    system("bundle exec rake sitemap:refresh RAILS_ENV=#{Rails.env}")
-    
-    render plain: "ok"
+    success = system("bundle exec rake sitemap:refresh RAILS_ENV=#{Rails.env} > /dev/null 2>&1")
+
+    if success
+      head :ok
+    else
+      head :internal_server_error
+    end
   end
 end
